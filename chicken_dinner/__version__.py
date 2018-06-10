eb08bb07209a5f79fb5622a8e9a5cf6fde9524e0
crflynn <crf204@gmail.com>
2018-06-10 18:57:18 -0400

    bump version to 0.1.2

--- chicken_dinner/__version__.py
+++ chicken_dinner/__version__.py
@@ -2,7 +2,7 @@
 __title__ = "chicken_dinner"
 __description__ = "PUBG JSON API wrapper and playback visualizer."
 __url__ = "https://github.com/crflynn/chicken-dinner"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Christopher Flynn"
 __author_email__ = "crf204@gmail.com"
 __license__ = "MIT"
