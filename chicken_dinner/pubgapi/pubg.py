2a77ab6706de8cb0b87172ff5e7e4d25811f36fb
crflynn <crf204@gmail.com>
2018-07-22 15:37:29 -0400

    actually fix the docs for tournaments in core

--- chicken_dinner/pubgapi/pubg.py
+++ chicken_dinner/pubgapi/pubg.py
@@ -185,13 +185,13 @@ class PUBG(object):
         """Get a tournament by its id.
 
         :param str tournament_id: the tournament id for which to get data
-        :return: a :class:`chicken_dinner.models.tournament.Tournament` object
+        :return: a :class:`chicken_dinner.models.Tournament` object
         """
         return Tournament(self, tournament_id)
 
     def tournaments(self):
         """Get a list of tournaments.
 
-        :return: a :class:`chicken_dinner.models.tournament.Tournaments` object
+        :return: a :class:`chicken_dinner.models.Tournaments` object
         """
         return Tournaments(self)
